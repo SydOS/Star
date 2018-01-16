9d3e93d10a7b7d51a92e0e41bf3a2e4b295dd560
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-16 13:50:38 -0800

    Code to send End of Interrupt to PIC

--- pic.c
+++ pic.c
@@ -1,1 +1,11 @@
 #include "main.h"
+#include "pic.h"
+#include "io.h"
+
+void PIC_sendEOI(unsigned char irq) {
+	if(irq >= 8) {
+		outb(PIC2_COMMAND, PIC_EOI);
+	}
+
+	outb(PIC1_COMMAND, PIC_EOI);
+}
---
+++ pic.h
@@ -0,0 +1,10 @@
+#define PIC1         0x20
+#define PIC2         0xA0
+#define PIC1_COMMAND PIC1
+#define PIC1_DATA    (PIC1+1)
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA    (PIC2+1)
+
+#define PIC_EOI      0x20
+
+extern void PIC_sendEOI(unsigned char irq);
