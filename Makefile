1f4ac9f8022c238d90ed7729093e566c160ed850
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-16 14:34:59 -0800

    Disable NMI

--- Makefile
+++ Makefile
@@ -7,8 +7,11 @@ all:
 	i686-elf-gcc -c vga.c -o vga.o $(CFLAGS)
 	i686-elf-gcc -c floppy.c -o floppy.o $(CFLAGS)
 	i686-elf-gcc -c pic.c -o pic.o $(CFLAGS)
+	i686-elf-gcc -c driver/nmi.c -o nmi.o $(CFLAGS)
 
 	i686-elf-gcc -T linker.ld -o sydos.bin -ffreestanding -O2 -nostdlib *.o -lgcc
+
+	rm -rf *.o
 
 clean:
 	rm -rf *.o *.bin
--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "main.h"
 #include "driver/vga.h"
 #include "driver/floppy.h"
+#include "drivers/nmi.h"
 
 void kernel_main(void) {
 	vga_initialize();
 	vga_writes("SydOS Pre-Alpha\n");
 	vga_writes("Starting up...\n");
+	vga_writes("Detecting floppy disks...\n")
 	floppy_detect();
+	vga_writes("Disabling NMI...\n");
+	NMI_disable();
 }
