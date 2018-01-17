18920466ac4d8aa2122e293235e561c7e08d2c2c
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-16 18:00:25 -0800

    This file is randomly changed for some reason

--- driver/floppy.c
+++ driver/floppy.c
@@ -11,9 +11,9 @@ void floppy_detect() {
 	b = c & 0xF; // Get low nibble by ANDing out low nibble
 
 	char *drive_type[6] = { "no floppy drive", "360kb 5.25in floppy drive", "1.2mb 5.25in floppy drive", "720kb 3.5in", "1.44mb 3.5in", "2.88mb 3.5in"};
-	vga_writes("Floppy drive A is an:\n");
+	vga_writes("Floppy drive A: ");
 	vga_writes(drive_type[a]);
-	vga_writes("\nFloppy drive B is an:\n");
+	vga_writes("\nFloppy drive B: ");
 	vga_writes(drive_type[b]);
 	vga_writes("\n");
 }
