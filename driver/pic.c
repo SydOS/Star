2c71acd880258987561ee8586ea0d823b916276a
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-16 18:01:05 -0800

    PIC driver

---
+++ driver/pic.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include "driver/pic.h"
+#include "io.h"
+
+void PIC_sendEOI(unsigned char irq) {
+	if(irq >= 8) {
+		outb(PIC2_COMMAND, PIC_EOI);
+	}
+
+	outb(PIC1_COMMAND, PIC_EOI);
+}
+
+/*
+	offset1: vector offset for master PIC
+		vectors on the master become offset1...offset1+7
+	offset2: same for slave PIC: offset2...offset2+7
+*/
+void PIC_remap(int offset1, int offset2) {
+	unsigned char a1, a2;
+
+	a1 = inb(PIC1_DATA);                     // Save masks
+	a2 = inb(PIC2_DATA);
+
+	outb(PIC1_COMMAND, ICW1_INIT+ICW1_ICW4); // Start init sequence in cascade mode
+	io_wait();
+	outb(PIC2_COMMAND, ICW1_INIT+ICW1_ICW4);
+	io_wait();
+
+	outb(PIC1_DATA, offset1);                // ICW2: Master PIC vector offset
+	io_wait();
+	outb(PIC2_DATA, offset2);                // ICW2: Slave PIC vector offset
+	io_wait();
+
+	outb(PIC1_DATA, 4);                      // ICW3: tell master PIC that slave exists at IRQ2
+	io_wait();
+	outb(PIC2_DATA, 2);                      // ICW3: tell slave PIC it's cascade identity
+	io_wait();
+
+	outb(PIC1_DATA, ICW4_8086);
+	io_wait();
+	outb(PIC2_DATA, ICW4_8086);
+	io_wait();
+
+	outb(PIC1_DATA, a1);
+	outb(PIC2_DATA, a2);
+}
