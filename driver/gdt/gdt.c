c60b089a3b9e67b6311215b5013491fd49dad99d
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-19 10:50:44 -0800

    GDT docs

--- driver/gdt/gdt.c
+++ driver/gdt/gdt.c
@@ -8,22 +8,31 @@ static uint32_t gdt_pointer = 0x806;
 static uint32_t gdtr_loc = 0x800;
 static uint32_t gdt_size = 0;
 
-int gdt_add_descriptor(uint8_t id, uint64_t desc) {
+/**
+ * Add a descriptor to the GDT table
+ * @param id   ID of GDT entry to add
+ * @param desc Description
+ */
+void gdt_add_descriptor(uint8_t id, uint64_t desc) {
 	uint32_t loc = gdt_pointer + sizeof(uint64_t)*id;
 	*(uint64_t*)loc = desc;
 	gdt_size += sizeof(desc);
-	return 0;
 }
 
-int gdt_set_descriptor() {
+/**
+ * Set a GDT entry
+ */
+void gdt_set_descriptor() {
 	*(uint16_t*)gdtr_loc = (gdt_size - 1) & 0x0000FFFF;
 	gdtr_loc += 2;
 	*(uint32_t*)gdtr_loc = gdt_pointer;
 	_set_gdtr();
 	_reload_segments();
-	return 0;
 }
 
+/**
+ * Initialize the GDT table
+ */
 void gdt_init() {
 	gdt_add_descriptor(0, 0);
 	gdt_add_descriptor(1, 0x00CF9A000000FFFF);
