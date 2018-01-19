d6a9e9a2df37274bf03964e162684296b55ad96f
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-19 13:43:59 -0800

    Update a20 driver to fit asm naming scheme

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #include "driver/nmi.h"
 #include "driver/pic.h"
 
-extern void enable_A20();
+extern void _enable_A20();
 
 /**
  * The main function for the kernel, called from boot.asm
@@ -16,10 +16,13 @@ void kernel_main(void) {
 	vga_writes("   _____           _  ____   _____ \n  / ____|         | |/ __ \\ / ____|\n | (___  _   _  __| | |  | | (___  \n  \\___ \\| | | |/ _` | |  | |\\___ \\ \n  ____) | |_| | (_| | |__| |____) |\n |_____/ \\__, |\\__,_|\\____/|_____/ \n          __/ |                    \n         |___/                     \n");
 	vga_writes("Copyright (c) Sydney Erickson 2017 - 2018\n");
 	vga_setcolor(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+
 	vga_writes("Initializing GDT...\n");
 	gdt_init();
+
 	vga_writes("Detecting floppy disks...\n");
 	floppy_detect();
+
 	vga_writes("Enabling NMI...\n");
 	NMI_enable();
 
@@ -30,7 +33,7 @@ void kernel_main(void) {
     );
     if (AX == 0) {
     	vga_writes("Enabling A20 line...\n");
-		enable_A20();
+		_enable_A20();
     } else if (AX == 1) {
     	vga_writes("A20 line already enabled.\n");
     } else {
