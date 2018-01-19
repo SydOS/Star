b718714dcb3d089868b9e3cad094e27a06f53357
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-19 09:51:47 -0800

    NMI docs

--- driver/nmi.c
+++ driver/nmi.c
@@ -1,10 +1,16 @@
 #include "main.h"
 #include "io.h"
 
+/**
+ * Enable non-maskable interrupts
+ */
 void NMI_enable() {
 	outb(0x70, inb(0x70)&0x7F);
 }
 
+/**
+ * Disable non-maskable interrupts
+ */
 void NMI_disable() {
 	outb(0x70, inb(0x70)|0x80);
 }
