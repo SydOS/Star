056f93fe06bcd9c235030a5f78bc0532db55e6b8
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-23 12:59:30 -0800

    add memory functions to header file

--- include/driver/memory.h
+++ include/driver/memory.h
@@ -6,3 +6,6 @@ typedef struct {
 extern char* malloc(size_t size);
 extern void memory_init(uint32_t kernel_end);
 extern void memory_print_out();
+extern void free(void *mem);
+extern void pfree(void *mem);
+extern char* pmalloc(size_t size);
