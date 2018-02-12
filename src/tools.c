00a7c85244fb51af3e282aac7687951539b80ff0
Sydney Erickson <sydneyerickson@icloud.com>
2018-02-12 08:02:16 -0800

    Random integer generation

--- src/tools.c
+++ src/tools.c
@@ -56,6 +56,20 @@ char* utoa(uint32_t value, char* result, int base) {
 	return result;
 }
 
+uint32_t random_seed = 1;
+
+uint32_t rand() {
+	random_seed = random_seed * 1103515245 + 12345;
+	return (uint32_t)(random_seed / 63336) % 4294967295;
+}
+
+uint32_t maxrand(uint32_t seed, uint32_t max) {
+	random_seed = random_seed + seed * 1103515245 + 12345;
+	return (uint32_t)(random_seed / 65536) % (max+1);
+}
+
+// -----------------------------------------------------------------------------
+
 size_t strlen(const char* str) {
 	size_t len = 0;
 	while (str[len]) { len++; }
