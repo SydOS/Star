ffe25744af8571f7354a99e67901311b5ac1bccc
Sydney Erickson <sydneyerickson@icloud.com>
2018-02-12 08:34:41 -0800

    Properly configure and calibrate floppy drives

--- driver/floppy.c
+++ driver/floppy.c
@@ -1,7 +1,11 @@
 #include <main.h>
+#include <tools.h>
 #include <io.h>
 #include <logging.h>
 #include <driver/floppy.h>
+#include <kernel/interrupts.h>
+
+bool irq_triggered = false;
 
 /**
  * Temporary function to detect floppy disks in drives
@@ -22,6 +26,23 @@ void floppy_detect() {
 	log("\n");
 }
 
+void floppy_wait_for_irq(uint16_t timeout) {
+	uint8_t ret = 0;
+	while (!irq_triggered) {
+		if(!timeout) break;
+		timeout--;
+		sleep(10);
+	}
+	if(irq_triggered) { ret = 1; } else { log("FLOPPY DRIVE IRQ TIMEOUT\n"); }
+	irq_triggered = false;
+	return ret;
+}
+
+void floppy_irq() {
+	irq_triggered = true;
+	//send_eoi(6);
+}
+
 unsigned char floppy_getversion() {
 	// Get version of floppy controller.
 	outb(FLOPPY_DATA_FIFO, FLOPPY_VERSION);
@@ -37,6 +58,16 @@ void floppy_reset() {
 void floppy_configure() {
 	// Send configure command.
 	outb(FLOPPY_DATA_FIFO, FLOPPY_CONFIGURE);
+	outb(FLOPPY_DATA_FIFO, 0x0);
+	char data = (1 << 6) | (1 << 5) | (0 << 4) | (15 - 1);
+	outb(FLOPPY_DATA_FIFO, data);
+	outb(FLOPPY_DATA_FIFO, 0x0);
+}
+
+void floppy_recalibrate(uint8_t drive) {
+	outb(FLOPPY_DATA_FIFO, FLOPPY_RECALIBRATE);
+	outb(FLOPPY_DATA_FIFO, drive);
+	floppy_wait_for_irq(300);
 }
 
 void floppy_init() {
@@ -49,7 +80,12 @@ void floppy_init() {
 	{
 		log("No floppy controller present, aborting!\n");
 		return;
+	} else if (version != 0x90) {
+		log("Unofficially supported floppy controller version. Proceed with caution!\n");
 	}
+
+	log("Installing floppy drive IRQ...\n");
+	interrupts_irq_install_handler(38, &floppy_irq);
 
 	// Print version for now.
 	utoa(version, temp1, 16);
@@ -57,5 +93,16 @@ void floppy_init() {
 	log(temp1);
 	log("!\n");
 
+	// Set and lock base config
+	log("Configuring floppy drive controller...\n");
+	floppy_configure();
+	log("Locking floppy drive controller configuration...\n");
+	outb(FLOPPY_DATA_FIFO, FLOPPY_LOCK);
+
+	// Reset floppy drive
+	log("Resetting floppy drive controller...\n");
 	floppy_reset();
+
+	log("Recalibrating floppy drives...\n");
+	floppy_recalibrate(0x0);
 }
