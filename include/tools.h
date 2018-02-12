926cb62f0c4789b247c86b4ad33f6f7ed362cb88
Sydney Erickson <sydneyerickson@icloud.com>
2018-02-12 07:53:07 -0800

    Define sleep in tools header

--- include/tools.h
+++ include/tools.h
@@ -4,5 +4,6 @@
 extern char* itoa(int value, char* result, int base);
 extern char* utoa(uint32_t value, char* result, int base);
 extern size_t strlen(const char* str);
+void sleep(uint32_t ms);
 
 #endif
