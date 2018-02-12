16776e76fc20c0bd0daeb361cc7648f1d294fc11
Sydney Erickson <sydneyerickson@icloud.com>
2018-02-12 12:23:48 -0800

    More memory functions

--- kernel/memory.c
+++ kernel/memory.c
@@ -172,3 +172,38 @@ char* pmalloc(size_t size)
 	}
 	return 0;
 }
+
+void* memcpy(const void* dest, const void* src, size_t count )
+{
+	// multitasking
+	//mutex_lock(&m_memcpy);
+	char* dst8 = (char*)dest;
+	char* src8 = (char*)src;
+
+	if (count & 1) {
+		dst8[0] = src8[0];
+		dst8 += 1;
+		src8 += 1;
+	}
+
+	count /= 2;
+	while (count--) {
+		dst8[0] = src8[0];
+		dst8[1] = src8[1];
+
+		dst8 += 2;
+		src8 += 2;
+	}
+
+	// multitasking
+	//mutex_unlock(&m_memcpy);
+	return (void*)dest;
+}
+
+void* memset16 (void *ptr, uint16_t value, size_t num)
+{
+	uint16_t* p = ptr;
+	while(num--)
+		*p++ = value;
+	return ptr;
+}
