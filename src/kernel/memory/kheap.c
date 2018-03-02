fad29b34026b23021784a4af33493015c5946e7f
John Davis <goldfishnet@aol.com>
2018-03-01 21:41:03 -0600

    Heap initialization tweak

--- src/kernel/memory/kheap.c
+++ src/kernel/memory/kheap.c
@@ -5,29 +5,29 @@
 #include <kernel/pmm.h>
 #include <kernel/kheap.h>
 
-static page_t currentKernelHeapEnd;
+static page_t currentKernelHeapSize;
 
 void kheap_init() {
-    currentKernelHeapEnd = KHEAP_VIRTUAL_ADDRESS_START + PAGE_SIZE_4M;
-
-    for (page_t i = KHEAP_VIRTUAL_ADDRESS_START; i <= currentKernelHeapEnd; i+=PAGE_SIZE_4K)
+    // Start with 4MB heap.
+    currentKernelHeapSize = PAGE_SIZE_4M;
+    for (page_t i = KHEAP_VIRTUAL_ADDRESS_START; i <= KHEAP_VIRTUAL_ADDRESS_START + currentKernelHeapSize; i+=PAGE_SIZE_4K)
         paging_map_kernel_virtual_to_phys(i, pmm_pop_page());
 
     // Test heap area.
-    kprintf("Testing %uKB of heap memory...\n", (currentKernelHeapEnd - KHEAP_VIRTUAL_ADDRESS_START) / 1024);
+    kprintf("Testing %uKB of heap memory...\n", currentKernelHeapSize / 1024);
     page_t *testBuffer = (page_t*)KHEAP_VIRTUAL_ADDRESS_START;
-    for (page_t i = 0; i < (currentKernelHeapEnd - KHEAP_VIRTUAL_ADDRESS_START) / sizeof(page_t); i++)
+    for (page_t i = 0; i < currentKernelHeapSize / sizeof(page_t); i++)
         testBuffer[i] = i;
 
     bool pass = true;
-    for (page_t i = 0; i < (currentKernelHeapEnd - KHEAP_VIRTUAL_ADDRESS_START) / sizeof(page_t); i++)
+    for (page_t i = 0; i < currentKernelHeapSize / sizeof(page_t); i++)
         if (testBuffer[i] != i) {
             pass = false;
             break;
         }
     kprintf("Test %s!\n", pass ? "passed" : "failed");
     if (!pass)
-        panic("Memory test of virtual address 0x1000 failed.\n");
+        panic("Memory test of heap failed.\n");
 
-    kprintf("Kernel heap initialized!\n");
+    kprintf("Kernel heap at 0x%X with a size of %uKB initialized!\n", KHEAP_VIRTUAL_ADDRESS_START, currentKernelHeapSize / 1024);
 }
