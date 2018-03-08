c1bbcda526e2e8874e019ad74c02df7bb8ff804c
John Davis <goldfishnet@aol.com>
2018-03-08 12:00:15 -0600

    Fix PIT

--- src/include/kernel/pit.h
+++ src/include/kernel/pit.h
@@ -1,6 +1,8 @@
+#ifndef PIT_H
+#define PIT_H
+
 // PIT I/O ports.
-enum
-{
+enum {
     PIT_PORT_CHANNEL0               = 0x40, // Channel 0 port (read/write). Tied to IRQ0.
     PIT_PORT_CHANNEL1               = 0x41, // Channel 1 port (read/write). Tied to DRAM (doesn't exist now).
     PIT_PORT_CHANNEL2               = 0x42, // Channel 2 port (read/write). Tied to PC speaker.
@@ -8,16 +10,14 @@ enum
 };
 
 // PIT counters.
-enum
-{
+enum {
     PIT_CMD_COUNTER0                = 0x00, // Channel 0 (IRQ0).
     PIT_CMD_COUNTER1                = 0x40, // Channel 1 (DRAM).
     PIT_CMD_COUNTER2                = 0x80, // Channel 2 (PC speaker).
 };
 
 // PIT access modes.
-enum
-{
+enum {
     PIT_CMD_ACCESS_LATCH            = 0x00, // Latch count value command.
     PIT_CMD_ACCESS_LSBONLY          = 0x10, // Access mode: lobyte only.
     PIT_CMD_ACCESS_MSBONLY          = 0x20, // Access mode: hibyte only.
@@ -25,8 +25,7 @@ enum
 };
 
 // PIT operating modes.
-enum
-{
+enum {
     PIT_CMD_MODE_TERMINALCOUNT      = 0x0, // Mode 0 (interrupt on terminal count).
     PIT_CMD_MODE_ONESHOT            = 0x2, // Mode 1 (hardware re-triggerable one-shot).
     PIT_CMD_MODE_RATEGEN            = 0x4, // Mode 2 (rate generator).
@@ -36,24 +35,23 @@ enum
 };
 
 // PIT binary modes.
-enum
-{
+enum {
     PIT_CMD_BINCOUNT_BINARY         = 0x0, // 16-bit binary.
     PIT_CMD_BINCOUNT_BCD            = 0x1  // Four-digit BCD.
 };
 
 // PIT command masks.
-enum
-{
+enum {
     PIT_CMD_MASK_BINCOUNT           = 0x01,
     PIT_CMD_MASK_MODE               = 0x0E,
     PIT_CMD_MASK_ACCESS             = 0x30,
     PIT_CMD_MASK_COUNTER            = 0xC0
 };
 
+#define PIT_BASE_FREQ 1193182
+
 extern uint64_t pit_ticks();
 extern void pit_init();
-
-extern void pit_set_task(uint8_t i);
-extern void pit_enable_tasking();
 extern void pit_startcounter(uint32_t freq, uint8_t counter, uint8_t mode);
+
+#endif
--- src/kernel/pit.c
+++ src/kernel/pit.c
@@ -19,18 +19,18 @@ uint64_t pit_ticks() {
 // Starts a counter on the PIT.
 void pit_startcounter(uint32_t freq, uint8_t counter, uint8_t mode) {
 	// Determine divisor.
-	uint16_t divisor = 1193180 / freq;
+	uint16_t divisor = PIT_BASE_FREQ / freq;
 
 	// Send command to PIT.
 	uint8_t command = 0;
+	command = (command & ~PIT_CMD_MASK_BINCOUNT) | PIT_CMD_BINCOUNT_BINARY;
 	command = (command & ~PIT_CMD_MASK_MODE) | mode;
 	command = (command & ~PIT_CMD_MASK_ACCESS) | PIT_CMD_ACCESS_DATA;
 	command = (command & ~PIT_CMD_MASK_COUNTER) | counter;
 	outb(PIT_PORT_COMMAND, command);
 
 	// Send divisor.
-	switch (counter)
-	{
+	switch (counter) {
 		case PIT_CMD_COUNTER0:
 			outb(PIT_PORT_CHANNEL0, divisor & 0xFF);
 			outb(PIT_PORT_CHANNEL0, (divisor >> 8) & 0xFF);
@@ -49,28 +49,26 @@ void pit_startcounter(uint32_t freq, uint8_t counter, uint8_t mode) {
 }
 
 // Callback for PIT channel 0 on IRQ0.
-static void pit_callback(registers_t *regs)
-{	
+static void pit_callback(registers_t *regs) {	
 	// Increment the number of ticks.
 	ticks++;
 
-	// Change tasks.
-	tasking_tick(regs);
+	// Change tasks every 5ms.
+	if (ticks % 5 == 0)
+		tasking_tick(regs);
 }
 
 // Initialize the PIT.
 void pit_init() {
-	// Start main timer at 1 tick = 1 ms.
-	pit_startcounter(1193, PIT_CMD_COUNTER0, PIT_CMD_MODE_RATEGEN);
+	// Start main timer at 1 tick = 2 ms.
+	pit_startcounter(1000, PIT_CMD_COUNTER0, PIT_CMD_MODE_SQUAREWAVEGEN);
 
 	// Register the handler with IRQ 0.
     interrupts_irq_install_handler(0, pit_callback);
 
     // Wait for a tick to happen
     kprintf("Waiting for PIT test...\n");
-	while (ticks == 0) {
-
-	}
+	while (ticks == 0);
 
 	kprintf("PIT initialized!\n");
 }
