58df13fe03a17148a0a6c7a184c603fce4f1b175
John Davis <goldfishnet@aol.com>
2018-03-10 12:26:49 -0600

    Base for SMP

---
+++ src/arch/i386/kernel/interrupts/smp.c
@@ -0,0 +1,9 @@
+#include <main.h>
+#include <kprint.h>
+#include <io.h>
+#include <arch/i386/kernel/lapic.h>
+#include <arch/i386/kernel/acpi.h>
+
+void smp_init() {
+    
+}
---
+++ src/include/arch/i386/kernel/smp.h
@@ -0,0 +1,8 @@
+#ifndef SMP_H
+#define SMP_H
+
+#include <main.h>
+
+extern void smp_init();
+
+#endif
