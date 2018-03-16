9acafa42ef916585f5c3ee447ab1101a58a0353f
Sydney Erickson <sydneyerickson@icloud.com>
2018-03-16 09:40:43 -0700

    Detect PCI device's other functions if it has more than one

--- src/driver/pci.c
+++ src/driver/pci.c
@@ -24,7 +24,7 @@ uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t o
     return (tmp);
 }
 
-struct PCIDevice* pci_check_device(uint8_t bus, uint8_t device) {
+struct PCIDevice* pci_check_device(uint8_t bus, uint8_t device, uint8_t function) {
 	uint16_t vendorID = pci_config_read_word(bus,device,0,PCI_VENDOR_ID);
 	uint16_t deviceID = pci_config_read_word(bus,device,0,PCI_DEVICE_ID);
 	uint16_t classInfo = pci_config_read_word(bus,device,0,PCI_SUBCLASS);
@@ -39,23 +39,29 @@ struct PCIDevice* pci_check_device(uint8_t bus, uint8_t device) {
     this_device->Subclass = (classInfo & ~0xFF00);
     this_device->HeaderType = headerType;
 
-	kprintf("PCI device: %X:%X | Class %X Sub %X | Bus %d Device %d\n", 
+	kprintf("PCI device: %X:%X | Class %X Sub %X | Bus %d Device %d Function %d\n", 
         this_device->VendorID, this_device->DeviceID, this_device->Class, 
-        this_device->Subclass, bus, device);
+        this_device->Subclass, bus, device, function);
+
+    if((this_device->HeaderType & 0x80) != 0) {
+        struct PCIDevice *sub_device = pci_check_device(bus, device, function++);
+        kheap_free(sub_device);
+    }
     
     return this_device;
 }
 
 void pci_check_busses(uint8_t bus) {
     bool onlyOneBus = true;
-    struct PCIDevice *host_device = pci_check_device(0, 0);
+    struct PCIDevice *host_device = pci_check_device(0, 0, 0);
     if((host_device->HeaderType & 0x80) != 0) {
         onlyOneBus = false;
     }
+    kheap_free(host_device);
 
 	// Check each device on bus
 	for (uint8_t device = 0; device < 32; device++) {
-		struct PCIDevice *this_device = pci_check_device(bus, device);
+		struct PCIDevice *this_device = pci_check_device(bus, device, 0);
         
         if(onlyOneBus == false) {
             if(this_device->Class == 6 && this_device->Subclass == 4) {
