77edcbebdfec0fbe2e916f84fcc68ace1e6e8678
Sydney Erickson <sydneyerickson@icloud.com>
2018-05-18 18:08:56 -0700

    L2Eth - add extern to function declaration in .h

--- src/include/kernel/networking/layers/l2-ethernet.h
+++ src/include/kernel/networking/layers/l2-ethernet.h
@@ -6,6 +6,6 @@ typedef struct {
 	// the memory size to be bigger than this actual struct to fit it.
 } __attribute__((packed)) ethernet_frame_t;
 
-ethernet_frame_t* l2_ethernet_create_frame(uint8_t* MACDest, uint8_t* MACSrc, 
+extern ethernet_frame_t* l2_ethernet_create_frame(uint8_t* MACDest, uint8_t* MACSrc, 
 							  uint16_t Ethertype, uint16_t payloadSize, 
 							  void* payloadPointer,  uint16_t *FrameSize);
