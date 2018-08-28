f00f30a8521a6e4cfc524714758b9b5917e374ba
Sydney Erickson <sydneyerickson@icloud.com>
2018-08-27 20:26:46 -0700

    Toolchain - i786 build scripts

---
+++ scripts/toolchain-i786-gdb.sh
@@ -0,0 +1,3 @@
+source global.sh
+
+download_compile https://ftp.gnu.org/gnu/gdb/gdb-$GDB_VERSION.tar.gz gdb-$GDB_VERSION "--target=i786-elf"
---
+++ scripts/toolchain-i786.sh
@@ -0,0 +1,4 @@
+source global.sh
+
+download_compile $GNU_MIRROR_BASE/binutils/binutils-$BINUTILS_VERSION.tar.gz binutils-$BINUTILS_VERSION "--with-isl=$INSTALL_DIR --with-cloog=$INSTALL_DIR --with-sysroot --disable-nls --disable-werror --target=i786-elf"
+download_compile $GNU_MIRROR_BASE/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.gz gcc-$GCC_VERSION "--with-isl=$INSTALL_DIR --with-cloog=$INSTALL_DIR --with-gmp=$INSTALL_DIR --with-mpfr=$INSTALL_DIR --with-mpc=$INSTALL_DIR --disable-nls --enable-languages=c,c++ --without-headers --target=i786-elf"
